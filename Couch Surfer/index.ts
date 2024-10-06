ef32a2f6da435b2f538ff2fbca0b742ffac566b4
Puso90 <juwishbrands@gmail.com>
2024-10-06 13:36:31 +0200

    restructuring files and making sure they reflect

---
+++ Couch Surfer/index.ts
@@ -0,0 +1,62 @@
+// Number Types mini-challenge 10 10.2
+// Write a function that will only accept numbers and attend to 
+// all TypeScript weakness flags.
+// : number
+const reviewTotalDisplay = document.querySelector('#reviews') 
+
+let Car
+
+const reviews = [
+    {
+        name: 'Sheia',
+        stars: 5,
+        loyaltyUser: true,
+        date: '01-04-2021'
+    },
+    {
+        name: 'Andrzej',
+        stars: 3,
+        loyaltyUser: false,
+        date: '28-03-2021'
+    },
+    {
+        name: 'Omar',
+        stars: 4,
+        loyaltyUser: true,
+        date: '27-03-2021'
+    },
+]
+
+function showReviewTotal(value: number, reviewer: string, isLoyalty: boolean) {
+    const iconDisplay = isLoyalty ? '⭐' : ''
+    reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + ' ' + iconDisplay
+}
+
+showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
+
+
+
+
+const you: {
+    firstName: string;
+    lastName: string;
+    isReturning: boolean;
+    age: number;
+    stayedAt: string[];
+} = {
+    firstName: 'Puso',
+    lastName: 'Ramoroa',
+    isReturning: true,
+    age: 34,
+    stayedAt: ['phokeng', 'cape-flats', 'tokyo']
+}
+
+
+function populateUser(isReturning : boolean, userName: string ) {
+    if (isReturning){
+        returningUserDisplay.innerHTML = 'back'
+    }
+    userNameDisplay.innerHTML = userName
+}
+
+populateUser(you.isReturning, you.firstName)
